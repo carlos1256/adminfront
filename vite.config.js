dcaeac2e545b4a7ff55e862f91d49231cf2c5ed6
Carlos Verano <Carlosverano1011@gmail.com>
2023-11-17 11:59:17 -0400

    primer deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/carlos1256/adminfront.git",
+  base: "/adminfront/",
 })
+ 
